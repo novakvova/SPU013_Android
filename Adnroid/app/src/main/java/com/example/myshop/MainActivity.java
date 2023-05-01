8cab2a709a64ebe26564e9a7f633ecf1af78a87c
novakvova <novakvova@gmail>
2023-05-01 15:44:56 +0300

    Working app

--- Adnroid/app/src/main/java/com/example/myshop/MainActivity.java
+++ Adnroid/app/src/main/java/com/example/myshop/MainActivity.java
@@ -22,7 +22,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     CategoriesAdapter adapter;
     RecyclerView rc;
