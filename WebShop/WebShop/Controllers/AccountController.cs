23a972e21d3e7445179bed94f29551ce9d89e2ae
novakvova <novakvova@gmail>
2023-05-06 12:54:42 +0300

    Add User and Register

---
+++ WebShop/WebShop/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Constants;
+using WebShop.Data.Entites.Identity;
+using WebShop.Helpers;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<UserEntity> _userManager;
+
+        public AccountController(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost("register")] 
+        public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model)
+        {
+            var imageName = string.Empty;
+            if(!string.IsNullOrEmpty(model.ImageBase64))
+            {
+                imageName = ImageWorker.SaveImage(model.ImageBase64);
+            }
+            UserEntity user = new UserEntity()
+            {
+                LastName = model.LastName,
+                FirstName = model.FirstName,
+                Email = model.Email,
+                Image = imageName,
+                UserName = model.Email
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                result = await _userManager.AddToRoleAsync(user, Roles.User);
+                return Ok();
+            }
+            
+            return BadRequest();
+        }
+    }
+}
--- WebShop/WebShop/Data/SeederDB.cs
+++ WebShop/WebShop/Data/SeederDB.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Constants;
+using WebShop.Data.Entites.Identity;
 
 namespace WebShop.Data
 {
@@ -11,6 +14,41 @@ namespace WebShop.Data
             {
                 var context = scope.ServiceProvider.GetRequiredService<AppEFContext>();
                 context.Database.Migrate();
+
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+
+                if(!context.Roles.Any())
+                {
+                    RoleEntity admin = new RoleEntity()
+                    {
+                        Name=Roles.Admin
+                    };
+
+                    RoleEntity user = new RoleEntity()
+                    {
+                        Name = Roles.User
+                    };
+                    var result = roleManager.CreateAsync(admin).Result;
+                    result = roleManager.CreateAsync(user).Result;
+                }
+                
+                if(!context.Users.Any())
+                {
+                    UserEntity user
+                        = new UserEntity()
+                        {
+                            FirstName = "Іван",
+                            LastName = "Марко",
+                            Email = "marko@gmail.com",
+                            UserName = "marko@gmail.com"
+                        };
+                    var result = userManager.CreateAsync(user, "123456").Result;
+                    if(result.Succeeded)
+                    {
+                        result = userManager.AddToRoleAsync(user, Roles.Admin).Result;
+                    }
+                }
             }
         }
     }
