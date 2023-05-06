025f78a5485f3167c06c3d8e2cbbe27c9341cada
novakvova <novakvova@gmail>
2023-05-06 09:51:32 +0300

    Add Delete and Upate category

--- WebShop/WebShop/Controllers/CategoriesController.cs
+++ WebShop/WebShop/Controllers/CategoriesController.cs
@@ -33,6 +33,20 @@ namespace WebShop.Controllers
            
             return Ok(model);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var model = await _appEFContext.Categories
+                .Where(x => x.IsDeleted == false && x.Id == id)
+                .Select(x => _mapper.Map<CategoryItemViewModel>(x))
+                .ToListAsync();
+            if(model.Count==0)
+                return NotFound();
+
+            return Ok(model[0]);
+        }
+
         [HttpPost("create")]
         public async Task<IActionResult> Create([FromBody] CategoryCreateItemVM model)
         {
@@ -49,6 +63,49 @@ namespace WebShop.Controllers
 
                 return BadRequest(new { error = ex.Message });
             }
+        }
+
+        [HttpPut("edit")]
+        public async Task<IActionResult> Edit([FromBody] CategoryEditItemVM model)
+        {
+            try
+            {
+                var cat = await _appEFContext.Categories.FindAsync(model.Id);
+                if(cat==null)
+                    return NotFound();
+                else
+                {
+                    cat.Name = model.Name;
+                    cat.Description = model.Description;
+                    cat.Priority= model.Priority;
+                    if(!string.IsNullOrEmpty(model.ImageBase64))
+                    {
+                        ImageWorker.RemoveImage(cat.Image);
+                        cat.Image = ImageWorker.SaveImage(model.ImageBase64);
+                    }
+                    
+                    _appEFContext.Categories.Update(cat);
+                    await _appEFContext.SaveChangesAsync();
+                    return Ok(_mapper.Map<CategoryItemViewModel>(cat));
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cat = await _appEFContext.Categories.FindAsync(id);
+            if (cat == null)
+                return NotFound();
+            cat.IsDeleted = true;
+            _appEFContext.SaveChanges();
+            return Ok();
         }
     }
 }
--- WebShop/WebShop/Models/CategoryViewModels.cs
+++ WebShop/WebShop/Models/CategoryViewModels.cs
@@ -4,10 +4,19 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Priority { get; set; }
         public string Image { get; set; }
     }
     public class CategoryCreateItemVM
     {
+        public string Name { get; set; }
+        public string ImageBase64 { get; set; }
+        public int Priority { get; set; }
+        public string Description { get; set; }
+    }
+    public class CategoryEditItemVM
+    {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string ImageBase64 { get; set; }
         public int Priority { get; set; }
